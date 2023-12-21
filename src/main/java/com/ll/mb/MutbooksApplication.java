aaad62d2cbc7b4721c3b9384d6d4c2cf976e2b80
itonse <govl3dnjs@gmail.com>
2023-12-21 22:17:34 +0900

    feat : BaseEntity 추가

--- src/main/java/com/ll/mb/MutbooksApplication.java
+++ src/main/java/com/ll/mb/MutbooksApplication.java
@@ -1,9 +1,11 @@
-package com.ll.mutbooks;
+package com.ll.mb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MutbooksApplication {
 
     public static void main(String[] args) {
