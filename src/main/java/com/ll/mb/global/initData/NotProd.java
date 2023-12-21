536889a69aeb3095845f5a8490b2729f4e5e8a0a
itonse <govl3dnjs@gmail.com>
2023-12-22 02:03:36 +0900

    feat : 캐시 기능 구현
    캐시 변동을 기록하는 CashLog 생성

--- src/main/java/com/ll/mb/global/initData/NotProd.java
+++ src/main/java/com/ll/mb/global/initData/NotProd.java
@@ -2,6 +2,7 @@ package com.ll.mb.global.initData;
 
 import com.ll.mb.domain.book.book.entity.Book;
 import com.ll.mb.domain.book.book.service.BookService;
+import com.ll.mb.domain.cash.cash.entity.CashLog;
 import com.ll.mb.domain.member.member.entity.Member;
 import com.ll.mb.domain.member.member.service.MemberService;
 import com.ll.mb.domain.product.cart.service.CartService;
@@ -43,12 +44,12 @@ public class NotProd {
         Member memberUser2 = memberService.join("user2", "1234").getData();
         Member memberUser3 = memberService.join("user3", "1234").getData();
 
-        Book book1 = bookService.createBook(memberUser1, "책 제목 1", "책 내용 1", 10000);
-        Book book2 = bookService.createBook(memberUser2, "책 제목 2", "책 내용 2", 10000);
-        Book book3 = bookService.createBook(memberUser2, "책 제목 3", "책 내용 3", 10000);
-        Book book4 = bookService.createBook(memberUser3, "책 제목 4", "책 내용 4", 10000);
-        Book book5 = bookService.createBook(memberUser3, "책 제목 5", "책 내용 5", 10000);
-        Book book6 = bookService.createBook(memberUser3, "책 제목 6", "책 내용 6", 10000);
+        Book book1 = bookService.createBook(memberUser1, "책 제목 1", "책 내용 1", 10_000);
+        Book book2 = bookService.createBook(memberUser2, "책 제목 2", "책 내용 2", 20_000);
+        Book book3 = bookService.createBook(memberUser2, "책 제목 3", "책 내용 3", 30_000);
+        Book book4 = bookService.createBook(memberUser3, "책 제목 4", "책 내용 4", 40_000);
+        Book book5 = bookService.createBook(memberUser3, "책 제목 5", "책 내용 5", 15_000);
+        Book book6 = bookService.createBook(memberUser3, "책 제목 6", "책 내용 6", 20_000);
 
         Product product1 = productService.createProduct(book3);
         Product product2 = productService.createProduct(book4);
@@ -57,5 +58,8 @@ public class NotProd {
         cartService.addItem(memberUser1, product1);      // 회원이 상품을 장바구니에 담았다
         cartService.addItem(memberUser1, product2);
         cartService.addItem(memberUser1, product3);
+
+        // User1 회원에게 100,000 캐시가 들어왔다, 무통장입금으로 인해, 이 것의 관련 회원은 본인
+        memberService.addCash(memberUser1, 100_000, CashLog.EvenType.충전__무통장입금, memberUser1);
     }
 }
