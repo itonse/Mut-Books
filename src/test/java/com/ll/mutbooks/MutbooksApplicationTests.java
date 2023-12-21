ce713a6369f84659c2df1d64da17db143cf69df6
itonse <govl3dnjs@gmail.com>
2023-12-21 22:32:10 +0900

    feat : @ActiveProfiles("test")  활성화

--- src/test/java/com/ll/mutbooks/MutbooksApplicationTests.java
+++ src/test/java/com/ll/mutbooks/MutbooksApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.mutbooks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MutbooksApplicationTests {
 
     @Test
